2998abb83bc44da4f9537e0888f4c12f6b7ac588
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-07-14 13:56:23 -0500

    Creacion de componente estudiantes

---
+++ app/estudiantes.component.js
@@ -0,0 +1,26 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+//import { Component } from '@angular/core' es una libreria que permite exportar la linea 4 @Component
+var core_1 = require("@angular/core");
+//@Component (decorador) es una funcion que recibe metadata
+var EstudiantesComponent = (function () {
+    //export es para que podamos usar EstudiantesComponent en otros componentes (clases)
+    function EstudiantesComponent() {
+    }
+    EstudiantesComponent = __decorate([
+        core_1.Component({
+            selector: 'estudiantes',
+            template: '<h3>Lista de Estudiantes</h3>' //Inserta el template en el selector. Template es el html
+        })
+        //export es para que podamos usar EstudiantesComponent en otros componentes (clases)
+    ], EstudiantesComponent);
+    return EstudiantesComponent;
+}());
+exports.EstudiantesComponent = EstudiantesComponent;
+//# sourceMappingURL=estudiantes.component.js.map
---
+++ app/estudiantes.component.ts
@@ -0,0 +1,11 @@
+//import { Component } from '@angular/core' es una libreria que permite exportar la linea 4 @Component
+import { Component } from '@angular/core'
+
+//@Component (decorador) es una funcion que recibe metadata
+@Component({
+    selector: 'estudiantes', // Busca la etiqueta estudiantes dentro del html
+    template: '<h3>Lista de Estudiantes</h3>' //Inserta el template en el selector. Template es el html
+})
+
+//export es para que podamos usar EstudiantesComponent en otros componentes (clases)
+export class EstudiantesComponent{}
