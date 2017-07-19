ef12572ab45ad7f440b233ca4f76ed73cfd2c442
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-07-19 16:54:31 -0500

    Enlace de datos eventos

--- app/estudiantes.component.js
+++ app/estudiantes.component.js
@@ -13,13 +13,20 @@ var EstudiantesComponent = (function () {
     function EstudiantesComponent() {
         this.titulo = "Lista de estudiantes";
         this.estudiantes = ['Ivan', 'Luis', 'Ricardo'];
+        this.circle = true;
+        this.padding = true;
+        this.color = "blue";
     }
+    EstudiantesComponent.prototype.alerta = function () {
+        console.log('Hubo un evento');
+        alert('Esta es una alerta');
+    };
     EstudiantesComponent = __decorate([
         core_1.Component({
             selector: 'estudiante',
             //Inserta el template en el selector. Template es el html
             // {{ variable }} se conoce como interpolación y es para que reemplace variable por el texto
-            template: "<h3> {{ titulo }}</h3> \n                <ul>\n                    <li *ngFor=\"let estudiante of estudiantes\">{{ estudiante }}</li>\n                </ul>"
+            template: "<div [style.margin-left]=\"300\">\n                <h3> {{ titulo }}</h3> \n                <ul>\n                    <li *ngFor=\"let estudiante of estudiantes\">{{ estudiante }}</li>\n                </ul>\n                <img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe3K-rDvbrlmRfdEv0HG7c2O5LhdTEGdfF-TV4mlRh14-LrtS-rg\" [class.img-circle]=\"circle\"/>\n                <input type=\"text\" [(ngModel)]=\"titulo\"/>\n                <button (click)=\"alerta()\">Test</button>\n                </div>\n                "
         })
         //export es para que podamos usar EstudiantesComponent en otros componentes (clases)
     ], EstudiantesComponent);
--- app/estudiantes.component.ts
+++ app/estudiantes.component.ts
@@ -6,14 +6,28 @@ import { Component } from '@angular/core'
     selector: 'estudiante', // Busca la etiqueta estudiantes dentro del html
     //Inserta el template en el selector. Template es el html
     // {{ variable }} se conoce como interpolación y es para que reemplace variable por el texto
-    template: `<h3> {{ titulo }}</h3> 
+    template: `<div [style.margin-left]="300">
+                <h3> {{ titulo }}</h3> 
                 <ul>
                     <li *ngFor="let estudiante of estudiantes">{{ estudiante }}</li>
-                </ul>`
+                </ul>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe3K-rDvbrlmRfdEv0HG7c2O5LhdTEGdfF-TV4mlRh14-LrtS-rg" [class.img-circle]="circle"/>
+                <input type="text" [(ngModel)]="titulo"/>
+                <button (click)="alerta()">Test</button>
+                </div>
+                `
 })
 
 //export es para que podamos usar EstudiantesComponent en otros componentes (clases)
 export class EstudiantesComponent{
     titulo = "Lista de estudiantes";
     estudiantes = ['Ivan','Luis','Ricardo'];
+    circle = true;
+    padding = true;
+    color = "blue";
+
+    alerta(){
+        console.log('Hubo un evento');
+        alert('Esta es una alerta');
+    }
 }
